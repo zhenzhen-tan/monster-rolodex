ecc63778cd619766f54c2ad0204e492363edf8b6
Zhenzhen Tan <zhenzhen.tan@r3.com>
2020-09-30 14:30:28 -0400

    Updates

---
+++ precache-manifest.769f2e75e44d706f863a7608fb27dcce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbf4e02fe3438383ef8b8f079ca869c9",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "1a2a1349030a1568aadd",
+    "url": "/monster-rolodex/static/css/main.fb94b0db.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a2a1349030a1568aadd",
+    "url": "/monster-rolodex/static/js/main.6ae3359d.chunk.js"
+  },
+  {
+    "revision": "212570b3a5f291f8b8f3",
+    "url": "/monster-rolodex/static/js/runtime-main.4a41f6c3.js"
+  }
+]);
